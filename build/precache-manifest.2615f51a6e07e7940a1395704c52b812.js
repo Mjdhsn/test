8ba1995615d289570f2670e44bc41f6195ab9f67
Mjdhsn <108516853+Mjdhsn@users.noreply.github.com>
2022-09-19 01:42:45 +0200

    commit

---
+++ build/precache-manifest.2615f51a6e07e7940a1395704c52b812.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dac8c8a4567354e16a2cac84f9956e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "546c981d1d6a725b9454",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "dbbc52e2692072a2cbae",
+    "url": "/static/css/main.2467d093.chunk.css"
+  },
+  {
+    "revision": "546c981d1d6a725b9454",
+    "url": "/static/js/2.4dcf0c34.chunk.js"
+  },
+  {
+    "revision": "64ba42aef879712727ca7e88c3aa7ab8",
+    "url": "/static/js/2.4dcf0c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbc52e2692072a2cbae",
+    "url": "/static/js/main.66c29d23.chunk.js"
+  },
+  {
+    "revision": "05c33ce4d41b1ddf1cd6",
+    "url": "/static/js/runtime-main.e4bccf27.js"
+  },
+  {
+    "revision": "ea0ff53eea44d16ddf836903cdfec179",
+    "url": "/static/media/airbnb.ea0ff53e.png"
+  },
+  {
+    "revision": "7ee41b049b589263715c38e989217138",
+    "url": "/static/media/carousel-1.7ee41b04.png"
+  },
+  {
+    "revision": "b948f5f3722fd9613882802f4333f307",
+    "url": "/static/media/carousel-2.b948f5f3.png"
+  },
+  {
+    "revision": "050ab61f89c74ed3628aa01b9e400069",
+    "url": "/static/media/carousel-4.050ab61f.png"
+  },
+  {
+    "revision": "a626e1eb4ac2f3a362695e87b62e5377",
+    "url": "/static/media/carousel-5.a626e1eb.png"
+  },
+  {
+    "revision": "679ce71dd25797a35f37633383a2fe6d",
+    "url": "/static/media/google.679ce71d.png"
+  },
+  {
+    "revision": "f2ee7bd03cdb105e194295b66b87be46",
+    "url": "/static/media/workers.f2ee7bd0.png"
+  }
+]);
