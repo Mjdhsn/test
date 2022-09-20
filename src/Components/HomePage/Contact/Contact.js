c72382897df5cb50d71b971c558aef7f6e45b799
Mjdhsn <108516853+Mjdhsn@users.noreply.github.com>
2022-09-20 20:21:17 +0200

    update

--- src/Components/HomePage/Contact/Contact.js
+++ src/Components/HomePage/Contact/Contact.js
@@ -19,7 +19,7 @@ const Contact = () => {
                         <button className="btn btn-lg btn-dark px-5">Send</button>
                     </div>
                 </div>
-                <h6 className="text-center mt-5 pb-3 mb-0">Copyright &copy; {(new Date()).getFullYear()}</h6>
+                <h6 className="text-center mt-5 pb-3 mb-0">Copyright &copy; 2019</h6>
             </div>
         </section>
     );
